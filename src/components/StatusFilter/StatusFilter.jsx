ad69828e4e88d9bd7f4fe5a1010f0bfb223f3fbc
Roman Fedevych <fedevychr@gmail.com>
2024-04-24 20:49:02 +0300

    store

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,5 +1,5 @@
-import { Button } from '../Button/Button';
-import css from './StatusFilter.module.css';
+import { Button } from "../Button/Button";
+import css from "./StatusFilter.module.css";
 
 export const StatusFilter = () => {
   return (
